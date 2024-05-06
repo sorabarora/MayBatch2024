fa7d0e7223061fdf504ed21a42af4597fceddde0
Asus <Asus@LAPTOP-915NAFGL>
2024-05-06 15:52:07 +0530

    Second commit and push

---
+++ Construtor_Concepts/src/cc/Print_Message.java
@@ -0,0 +1,13 @@
+package cc;
+
+public class Print_Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to all of you");
+		System.out.println("Automation is very easy");
+
+	}
+
+}
